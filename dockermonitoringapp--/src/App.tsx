d524add980d0031018bff65220467516708c9d91
Egor Sazonov <147899660+EgorSazonov0405@users.noreply.github.com>
2025-02-05 23:32:25 +0300

    add input row

--- dockermonitoringapp--/src/App.tsx
+++ dockermonitoringapp--/src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useFetchContainers } from './hooks/useFetchContainers';
 import ContainerList from './components/ContainerList';
-
+import InputRow from './components/InputRow';
 
 const App: React.FC = () => {
-  // Используем хук для получения контейнеров с интервалом 5 секунд
   const { containers, loading, error } = useFetchContainers(5000);
+  const [data, setData] = useState<Entry[]>([]);
 
-  // Обработка состояния загрузки
+  const handleAdd = (newEntry: Entry) => {
+      setData([...data, newEntry]);
+  };
+
   if (loading) return <div>Loading...</div>;
 
-  // Обработка ошибок
   if (error) return <div>Error: {error}</div>;
 
-  // Основной интерфейс приложения
   return (
-    <div className="container mt-5">
+    <div className="main_container">
       <h1 className="text-center">Docker Container Monitor</h1>
-      <ContainerList containers={containers} />
+      <InputRow onAdd={handleAdd} />
+            <div className="data-list">
+                {data.map((entry, index) => (
+                    <div key={index} className="data-entry">
+                        <span>{entry.name}</span>
+                        <span>{entry.ipAddress}</span>
+                        <span>{entry.pingTime}</span>
+                        <span>{entry.status}</span>
+                    </div>
+                ))}
+            </div>
+        <ContainerList containers={containers} />
     </div>
   );
 };
